2a0f8fbdc529c5568f0ebd0824dc21516551aa63
muodilo <murindahabiodilo@gmail.com>
2025-06-12 16:33:53 +0200

    feat: add navigation items to Navbar

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,9 +1,25 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
     <nav className='px-16 py-3 border-b border-slate-300'>
      <div>Logo</div>
+
+     <ul>
+        <li>
+            <Link href={'/'}>Home</Link>
+        </li>
+        <li>
+            <Link href={'/'}>Jobs</Link>
+        </li>
+        <li>
+            <Link href={'/'}>Post aJob</Link>
+        </li>
+        <li>
+            <Link href={'/'}>Companies</Link>
+        </li>
+     </ul>
 
     </nav>
   )
